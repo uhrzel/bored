9c10777a5a7e481be4af65629ee49f9b5149fe0a
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-07-17 20:47:19 +0800

    edit

--- index.js
+++ index.js
@@ -1,51 +1,17 @@
-// dunno
-let im =
-  "Namamasko po! Mangarol si Arzel! We wish you a merry Christmas We wish you a merry Christmas We wish you a merry Christmas and a happy new year （￣︶￣）↗\n\n GCASH: 09090937257";
+let letter = "Hi guys san o kamo balik skwelahan kay manglibre ko tubig";
 
-// random characters
-let random_chars = [
-  "@",
-  "!",
-  "#",
-  "%",
-  "$",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "]",
-  "[",
-  "1",
-  "0",
-];
-
-$(function () {
-  const window_width = window.innerWidth;
-  const window_height = window.innerHeight;
-
-  let node = $(".mess");
-  let im_index = 0;
-  let current_len = "";
-  let is_done = false;
-
-  let interval_id = setInterval(() => {
-    if (im_index != im.length) {
-      current_len += im[im_index];
-      for (let i = 0; i < 30; i++) {
-        if (im_index == im.length - 1) node.text(current_len);
-        else
-          node.text(
-            current_len +
-              random_chars[Math.floor(Math.random() * random_chars.length)]
-          );
-      }
-    } else {
-      node.remove();
-      $("body").css("display: block;");
-      clearInterval(interval_id);
-      is_done = true;
+let symbols = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "[", "]", "{", "}"];
+let is_index = 0;
+let body = document.querySelector(".mess");
+let interval = setInterval(() => {
+  if (is_index < letter.length) {
+    let text = letter.slice(0, is_index + 1);
+    if (is_index < letter.length - 1) {
+      text += symbols[Math.floor(Math.random() * symbols.length)];
     }
-    im_index++;
-  }, 100);
-});
+    body.textContent = text;
+    is_index++;
+  } else {
+    clearInterval(interval);
+  }
+}, 100);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+  for (let index = 0; index < letter.length
